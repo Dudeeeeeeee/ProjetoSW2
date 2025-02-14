56de63bf8a70b0d88dde17fe7d6a2bd4cfd2258b
Dudeeeeeeee <mariaeduardafraga00@gmail.com>
2025-02-14 11:40:55 -0300

    14/02

--- index.php
+++ index.php
@@ -22,15 +22,35 @@
      <div class="row">
 
         <div class="col-sm-4">
-        <h1>Adimistrador</h1>
+        <h1 class="bg-primary text-warning">Administrador</h1>
+        <p class="text-primary">
+        utilize esta opção para realizar o cadastro de novos administradores, ou edita-los.
+
+        </p>
     </div>
         <div class="col-sm-4">
-        <h1>Categorias</h1>
+        <h1 class="bg-danger text-">Categorias</h1>
+        <p class="text-danger"> Utilize esta opção para cadastrar as categorias dos produtos que serão vendidos on-line.
+</p>
     </div>
         <div class="col-sm-4">
-        <H1>Fornecedor</h1>
+        <h1 class="bg-success">Fornecedor</h1>
+        <p class="text-success"> Utilize esta opção para cadastrar os fornecedores dos produtos que serão vendidos.</p>
     </div>
 </div>
+
+<div class="row">
+
+<div class="col-sm-6">
+    <h1>Produtos</h1>
+
+</div>
+<div class="col-sm-6">
+    <h1>Vendas</h1>
+
+</div>
+</div>
+
 </div>
 </body>
 </html>
