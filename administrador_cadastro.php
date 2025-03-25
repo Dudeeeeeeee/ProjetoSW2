a0c2cb351b5e3e5378b763d8894e18d5af7dc5ab
Dudeeeeeeee <mariaeduardafraga00@gmail.com>
2025-03-25 10:49:42 -0300

    25/03

--- administrador_cadastro.php
+++ administrador_cadastro.php
@@ -19,6 +19,7 @@
                 <h3 class="text-center p-3">Confirmação do Cadastro</h3>
             <div>
                 <?php 
+                include "conexao.php";
                 $nome = $_REQUEST["nome"];
                 $login = $_REQUEST["login"];
                 $senha = md5( $_REQUEST["senha"] );
@@ -29,7 +30,7 @@
                 $sql = "insert into administrador(nome, login, senha)
                 values (:nome, :login, :senha)";
 
-                include "conexao.php";
+              
                 $result = $conexao->prepare($sql);
                 $result->bindValue(":nome", $nome);
                 $result->bindValue(":login", $login);
